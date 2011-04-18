07e221357f9bbe02072140a91dbdb90950844be6
podhmo <ababjam61@gmail.com>
2011-04-18 16:27:25 +0900

    virtualenvに対応した。まだ、どんなmoduleがあるのかすぐには見れない。たぶん。

--- python-ex-util.el
+++ python-ex-util.el
@@ -1,3 +1,165 @@
 (require 'with-prefix)
+
+(eval-when-compile (require 'cl))
+
+(with-prefix 
+    ((@ python-ex-util:)
+     (util. peu:))
+
+  ;; utility
+  (defun util.tmp-file (&optional file)
+    "return uniq name in tmp-dirctory"
+    (cond ((null file) (util.tmp-file (format "peu:%s.py" (gensym))))
+          (t (let* ((begin-with-slash-p  (char-equal ?/ (aref file 0)))
+                    (file* (if begin-with-slash-p (substring-no-properties file 1 (length file)) file)))
+               (concat "/tmp/" file*)))))
+
+  (defun util.command-format-with-current-env (cmd)
+    "return format string branching venv or not"
+    (or (@and-let* ((env (@current-env-maybe)))
+          (format "(source %s && workon %s && %s %%s)" (@current-rc) env cmd))
+        (format "%s %%s" cmd)))
+
+  ;; default-functionを書き換えたいね。
+  (defun util.execute-command-with-current-env (cmd args &optional execute-function)
+    (let ((fun (or execute-function
+                   (lambda (&rest args) (message (apply 'shell-command-to-string args)))))
+          (args-str (if (listp args) (mapconcat 'identity args " ") args)) 
+          (cmd-format (util.command-format-with-current-env cmd)))
+      (funcall fun (format cmd-format args-str))))
+
+  (defmacro @with-lexical-bindings (syms &rest body)
+    (let ((clauses (loop for sym in syms collect (\` ((\, sym) (\, sym))))))
+      (\` (lexical-let ((\,@ clauses)) (\,@ body)))))
+  (put '@with-lexical-bindings 'lisp-indent-function 1)
+  (defmacro @with-gensyms (syms &rest body)
+    (let ((bindings (mapcar (lambda (x) (\` ((\, x) '(\, (gensym))))) syms)))
+      (\` (let ((\,@ bindings)) (\,@ body)))))
+  (put '@with-gensyms 'lisp-indent-function 1)
+  
+  (defmacro @rlet1 (var val &rest body)
+    "imported from gauche"
+    (\` (@let1 (\, var) (\, val) (\,@ body) (\, var))))
+  (put '@rlet1 'lisp-indent-function 2)
+  (defmacro @let1 (var val &rest body)
+    "imported from gauche"
+    (\` (let (((\, var) (\, val))) (\,@ body))))
+  (put '@let1 'lisp-indent-function 2)
+  (defmacro @and-let* (bindings &rest body)
+    "imported from srfi-2"
+    (reduce (function
+             (lambda (binding r)
+               (let ((head (car binding)))
+                 (cond ((and (atom head) (symbolp head))
+                        (\` (let ((\, binding)) (when (\, head) (\, r)))))
+                       ((listp head)
+                        (\` (when (\, head) (\, r))))
+                       (t (error "and-let*: invalid head %s" head))))))
+            bindings
+            :from-end
+            t
+            :initial-value
+            (\` (progn (\,@ body)))))
+  (put '@and-let* 'lisp-indent-function 1)
+  (defmacro @alambda (args &rest body)
+    "Anaphoric lambda. enable to self recursion using `self' anaphorar"
+    (\` (labels ((self (\, args) (\,@ body))) (function self))))
+  (put '@alambda 'lisp-indent-function 1)
+  (defmacro @aand (&rest args)
+    "Anaphoric and. anaphorar is `it'"
+    (cond ((null args)
+           t)
+          ((null (cdr args))
+           (car args))
+          (t (\` (@aif (\, (car args)) (@aand (\,@ (cdr args))))))))
+
+  (defmacro @aif (test-form then-form &rest else-forms)
+    "Anaphoric if. Temporary variable `it' is the result of test-form."
+    (\` (let ((it (\, test-form))) (if it (\, then-form) (\,@ else-forms)))))
+  (put '@aif 'lisp-indent-function 2)
+  
+    ;;; current-python
+  (defun @current-rc ()
+    (format "~/.%src" (file-name-nondirectory (getenv "SHELL"))))
+
+  (defun @current-env-maybe ()
+    (@and-let* ((workon-path (getenv "WORKON_HOME"))
+                (rx (format "%s/\\([^/]+\\)" (expand-file-name workon-path)))
+                ((string-match rx default-directory)))
+      (match-string 1 default-directory)))
+
+  (defun @current-python () 
+    (util.command-format-with-current-env "python"))
+
+  ;; interactive command using `python-ex-util:current-python'
+  (defun @describe-current-python () (interactive)
+    (message
+     (shell-command-to-string
+      (format (@current-python) "-c 'import os; os.system(\"which python\")'"))))
+
+  (defvar @tmp-file-auto-cleaning-p nil
+    "this variable is true, then, auto cleaning after using tmp-file(this occured is working on non-file buffer),")
+
+  (defun @eval-buffer-with-current-python (&optional file) (interactive "P")
+    (@let1 file (or file (buffer-file-name))
+      (cond ((null file)
+             (let ((bufstring (buffer-string))
+                   (file (util.tmp-file)))
+               (with-temp-file file
+                 (insert bufstring)
+                 (@eval-buffer-with-current-python file))
+               (when @tmp-file-auto-cleaning-p (delete-file file))))
+            (t
+             (compile (format (@current-python) file))))))
+
+    ;;; ffap from import module sentence
+  (defvar @module-tokens-regexp "[^ .]+\\(\\.[^ .]+\\)*")
+
+  (defun @module-tokens-in-current-line ()
+    "[maybe] return match object or nil"
+    (save-excursion
+      (let ((rx (format "^[ \t]*\\(from +\\(%s\\) +import +[^ ]+\\|import +\\(%s\\)\\)"
+                        @module-tokens-regexp
+                        @module-tokens-regexp
+                        )))
+        (goto-char (point-at-bol))
+        (and (re-search-forward rx  (point-at-eol) t 1)
+             (or (match-string-no-properties 4)
+                 (match-string-no-properties 2))))))
+
+
+  (defun @library-path-list ()
+    (let* ((cmd "import sys; D=[d for d in sys.path if not 'bin' in d]; print ','.join(D)")
+           (sys-paths-str (shell-command-to-string
+                           (format "%s -c \"%s\""
+                                   (@current-python)
+                                   cmd))))
+      (cdr (split-string sys-paths-str ","))))
+
+  (defun @module-name-from-path (module &optional force-reload-p)
+    (@let1 path (replace-regexp-in-string "\\." "/" module)
+      (loop for dir in (@library-path-list)
+            for d = (concat dir "/" path)
+            if (file-exists-p d)
+            return d
+            else
+            for file = (concat d ".py")
+            when (file-exists-p file)
+            return file)))
+
+  (defun @current-library-path ()
+    "[maybe] return current library path from import sentence"
+    (and-let* ((module (@module-tokens-in-current-line)))
+      (@module-name-from-path (@module-tokens-in-current-line))))
+
+  (defun @ffap/import-sentence (other-frame-p) (interactive "P")
+    "ffap for import sentence"
+    (and-let* ((path (@current-library-path)))
+      (cond (other-frame-p (find-file-other-frame path))
+            (t (find-file path)))))
+
+  )
+
 (provide 'python-ex-util)
+
 
--- with-prefix.el
+++ with-prefix.el
@@ -1,70 +1,34 @@
+;; == with-prefix.el ==
+
+;;;; how to use
+;; (with-prefix 
+;;     ((@ wp:)
+;;      (util. with-prefix-util:))
+  
+;;   (defun @odd? (x) 
+;;     "check a received argument is odd number or not"
+;;     (if (<= x 0) nil (@even? (- x 1))))
+  
+;;   (defun @even? (x)
+;;     (if (<= x 0) t (@odd? (- x 1))))
+
+;;   (defun util.out (&rest args) 
+;;     (message (prin1-to-string args)))
+
+;; (util.out (@odd? 9) 
+;;           (@even? 9) 
+;;           (funcall '@odd? 10)
+;;           (apply '@even? '(10))))
+
 ;; (setq debug-on-error t)
 (eval-when-compile (require 'cl))
 
-;; internal variables
-(defvar with-prefix-buffer-prefix-alist nil
-  "this variable is internal variable. don't change value.")
-
-
-(defmacro with-prefix (target replacement &rest body)
-  "with-prefix is pseudo-name-space(but roughly implement)
-
-use this::
-(with-prefix @ with-prefix: body ...)"
-
-  (declare (indent 0))
-  (let* ((target-str (if (symbolp target) (symbol-name target) target))
-         (target-rx (format "^%s" target-str))
-         (replacement-str (format "%s" replacement)))
-
-    `(progn
-       ;; add a relation  for describe-function
-       (add-to-list 'with-prefix-buffer-prefix-alist
-                    (list ,(buffer-name (current-buffer)) ,replacement-str ,target-str))
-       ;; replace
-       ,@(wp:tree-map-safe
-          (lambda (sym) 
-            (if (symbolp sym)
-                (intern
-                 (replace-regexp-in-string ;;slack off
-                  target-rx replacement-str
-                  (symbol-name sym)))
-                sym))
-          body))))
-
-;; a advice for finding function location
-(defadvice find-function-search-for-symbol
-  (after with-prefix-force-find last (symbol type library) activate)
-  (destructuring-bind (buf . pt) ad-return-value
-
-    ;; when a function definition point is not found
-    (unless pt
-      (let ((pat-rep-values-maybe 
-             (assoc-default (buffer-name buf) with-prefix-buffer-prefix-alist)))
-
-        ;; and a pattern-replacement relation is found
-        (when pat-rep-values-maybe
-          (destructuring-bind (pat rep) pat-rep-values-maybe
-            (let ((pat* (format "^%s" pat)))
-
-              ;; force finding the function definition
-              (with-current-buffer buf
-                (goto-char (point-min))
-                (and (search-forward (replace-regexp-in-string pat* rep (symbol-name symbol)) nil t)
-                     (setq ad-return-value (cons buf (point))))))))))))
-
 ;; utility
-(defun wp:mapcar-safe (fn maybe-list)
-  "mapcar enable to iterate maybe-list (include dot-list)"
-  (let ((r (list)) (xs maybe-list))
-    (condition-case e
-        (progn
-          (while (not (null xs))
-            (push (funcall fn (car xs)) r)
-            (setq xs (cdr xs)))
-          (nreverse r))
-      (error (rlet1 r* (nreverse r)
-               (setcdr (last r*) (funcall fn xs)))))))
+(defun wp:x-to-string (x)
+  (format "%s" x))
+
+(defun wp:x-to-prefix-regexp (x) 
+  (concat "^" (wp:x-to-string x)))
 
 (defun wp:tree-map-safe (fn tree)
   "`wp:mapcar-safe' recursive version"
@@ -74,20 +38,120 @@ use this::
                                   tree)))
       (rec tree))))
 
+(defun wp:mapcar-safe (fn maybe-list)
+  "mapcar enable to iterate maybe-list (include dot-list)"
+  (let ((r (list)) (xs maybe-list))
+    (condition-case e
+        (progn
+          (while (not (null xs))
+            (push (funcall fn (car xs)) r)
+            (setq xs (cdr xs)))
+          (nreverse r))
+      (wrong-type-argument 
+       (let ((r* (nreverse r)))
+         (setcdr (last r*) (funcall fn xs))
+         r*)))))
+
+
+(defmacro wp:and-let*  (bindings &rest body)
+  "imported from srfi-2"
+  (reduce (function
+           (lambda (binding r)
+             (let ((head (car binding)))
+               (cond ((and (atom head) (symbolp head))
+                      (\` (let ((\, binding)) (when (\, head) (\, r)))))
+                     ((listp head)
+                      (\` (when (\, head) (\, r))))
+                     (t (error "and-let*: invalid head %s" head))))))
+          bindings
+          :from-end
+           t
+           :initial-value
+            (\` (progn (\,@ body)))))
+(put 'wp:and-let* 'lisp-indent-function 1)
+
+
+;; internal variables
+(defvar with-prefix:buffer-prefix-relation-alist nil
+  "this variable is internal variable. don't change value.")
+
+
+(defun with-prefix:update-prefix-relations (bound-buf bindings)
+  (let* ((bufname (buffer-name bound-buf))
+        (relations (loop for (pat rep) in bindings
+                         collect `(,(wp:x-to-prefix-regexp rep) . ,pat)))
+        (alist (assoc bufname with-prefix:buffer-prefix-relation-alist)))
+    
+    (cond (alist (setf (cdr alist) (union (cdr alist) relations :test 'equal)))
+          (t (add-to-list 'with-prefix:buffer-prefix-relation-alist
+                          `(,bufname . ,relations))))))
+  
+(defmacro with-prefix (head &rest body)
+  "with-prefix is pseudo-name-space(but roughly implement)"
+  (declare (indent 1) (debug t))
+  
+  ;; binding from head
+  (let ((bindings
+         (mapcar (lambda (args)
+                   (mapcar 'wp:x-to-string args))
+                 head)))
+    ;; add a relation  for describe-function
+    (with-prefix:update-prefix-relations
+     (current-buffer)  bindings)
+
+    (lexical-let ((shortcut-rx-full-prefix-alist 
+                   (loop for (shortcut full-prefix) in bindings
+                         collect `(,(wp:x-to-prefix-regexp shortcut) . ,full-prefix))))
+
+    (labels ((%replace-shortcut-to-full-prefix
+              (elt)
+              (let ((replacement* (symbol-name elt)))
+                (loop for (shortcut . full-prefix) in shortcut-rx-full-prefix-alist
+                      do
+                      (setq replacement*
+                            (replace-regexp-in-string
+                             shortcut full-prefix replacement*)))
+                (intern replacement*))))
+
+             `(progn
+                ;; replace
+                ,@(wp:tree-map-safe
+                   (lambda (elt)
+                     (cond ((not (symbolp elt)) elt)
+                           (t (%replace-shortcut-to-full-prefix elt))))
+                   body))))))
+
+(defmacro with-prefix1 (target replacement &rest body)
+  (declare (indent 2) (debug t))
+  `(with-prefix ((,target ,replacement))
+                ,@body))
+
+;; a advice for finding function location
+(defadvice find-function-search-for-symbol
+  (after with-prefix-force-find last (symbol type library) activate)
+  (destructuring-bind (buf . pt) ad-return-value
+
+    ;; when a function definition point is not found
+    (unless pt
+      (wp:and-let*
+          ((pat-rep-values-maybe 
+            (assoc-default (buffer-name buf)
+                           with-prefix:buffer-prefix-relation-alist))
+           (pat-rep-pair-maybe
+            (assoc* (symbol-name symbol) pat-rep-values-maybe
+                    :test (lambda (str rx) (string-match rx str)))))
+
+        (destructuring-bind (pat . rep) pat-rep-pair-maybe
+          
+          ;; force finding the function definition
+          (with-current-buffer buf
+            (goto-char (point-min))
+            (and (search-forward 
+                  (replace-regexp-in-string 
+                   pat rep (symbol-name symbol)) nil t)
+                 (setq ad-return-value (cons buf (point))))))))))
 
 ;; ;; for debugging
-;; ;; (with-prefix 
-;; ;;   @ with-prefix:
-;; ;;     (defun @odd? (x) 
-;; ;;       "check a received argument is odd number or not"
-;; ;;       (if (<= x 0) nil (@even? (- x 1))))
-;; ;;     (defun @even? (x)
-;; ;;       (if (<= x 0) t (@odd? (- x 1))))
-
-;; ;;     (print (list '@odd? 10 (@odd? 10)))
-;; ;;     (print (list '@odd? 9 (funcall '@odd? 9)))
-;; ;;     (print (list '@even? 10 (apply '@even? '(10)))))
-
 
 ;; ;; (add-to-list 'load-path default-directory)
 ;; ;; (find-function-search-for-symbol 'with-prefix:odd\? nil "with-prefix.el")
